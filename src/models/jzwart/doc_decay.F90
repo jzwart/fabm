e18ee4bfa4f917f3aa2de35cddc51e66424c3d5d
jzwart <jzwart@usgs.gov>
2017-11-10 17:01:47 -0600

    tending to errors

--- src/models/jzwart/doc_decay.F90
+++ src/models/jzwart/doc_decay.F90
@@ -26,12 +26,11 @@ contains
       class (type_jzwart_doc_decay), intent(inout), target :: self
       integer,                          intent(in)            :: configunit
  
-      call self%reigster_dependency(self%id_wtr,'wtr','degrees_c','water_temperature')
-      call self%register_diagnostic_variable(self%id_decayrate,'decayrate','day-1','doc_decay_rate')
+      !call self%register_dependency(self%id_wtr,'wtr','degrees_c','water_temperature')
+      !call self%register_diagnostic_variable(self%id_decayrate,'decayrate','day-1','doc_decay_rate')
       call self%register_state_variable(self%id_doc,'doc','mol_c','dissolved_organic_carbon')
-      call self%add_to_aggregate_variable(standard_variable%total_carbon,self%id_doc)
       call self%get_parameter(self%d,'d','day-1','decay_rate_doc',0.002)
-      ! Register model parameters and variables here.
+      
    end subroutine initialize
 
    subroutine do(self,_ARGUMENTS_DO_)
@@ -48,14 +47,15 @@ contains
       _GET_(id_doc,doc)
       
       ! compute doc decay based on decay at 20 and water temperature 
-      doc_decay = d*1.047^(id_wtr-20)
-      ddoc_dt = self%doc_decay*doc
+      ! doc_decay = d*1.047**(id_wtr-20)
+      ! ddoc_dt = self%doc_decay*doc
+      ddoc_dt = self%d*doc
       
       ! send rates of change to FABM
       _SET_ODE_(self%id_doc,-ddoc_dt)
       
       ! send the value of diagnostic variables to FABM
-      _SET_DIAGNOSTIC_(self%id_decayrate,doc_decay)
+      ! _SET_DIAGNOSTIC_(self%id_decayrate,doc_decay)
       
       _LOOP_END_
    end subroutine do
